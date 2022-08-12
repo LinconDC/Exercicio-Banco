d7de8430ef38eefa356a71fa1794d609f398df10
Lincon Martins <linconmartins18@gmail.com>
2022-08-12 10:58:22 -0300

    Exercício Dio Banco - Java

---
+++ DioBanco/src/br/com/dio/banco/modelo/SeguroDeVida.java
@@ -0,0 +1,10 @@
+package br.com.dio.banco.modelo;
+
+public class SeguroDeVida implements Tributavel {
+
+	@Override
+	public double getValorImposto() {
+		return 42;
+	}
+
+}
---
+++ DioBanco/src/br/com/dio/banco/modelo/Tributavel.java
@@ -0,0 +1,6 @@
+package br.com.dio.banco.modelo;
+
+public interface Tributavel {
+
+	double getValorImposto();
+}
